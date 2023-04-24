d15df2c2874110bfdfc9a2b0867f6ff8c6ffc227
sumit-porwal <61969815+sumit-porwal@users.noreply.github.com>
2023-04-24 22:36:03 +0530

    Update Apply.jsx

--- src/module/Apply.jsx
+++ src/module/Apply.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import URL from './useQuery';
+import { URL } from './useQuery';
 const Apply = () => {
 	const navigate = useNavigate();
 	const [formData, setFormData] = useState({
